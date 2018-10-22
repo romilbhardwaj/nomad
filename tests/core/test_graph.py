8716bd3f75097f27e2ec744bb7911184cbc0cd3e
Alvin <alvin.ghouas@gmail.com>
2018-10-21 21:19:55 -0700

    Add tests/ folder

---
+++ tests/core/test_graph.py
@@ -0,0 +1,10 @@
+import unittest
+from core.graph.digraph import DirectedGraph 
+
+class TestGraphMethods(unittest.TestCase):
+
+    def test_create_grap(self):
+        g = DirectedGraph()
+
+if __name__ == '__main__':
+    unittest.main()        
