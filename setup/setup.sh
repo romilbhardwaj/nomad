216ba9d11ed2fc3227997f50e36246a9330c3629
Alvin <alvin.ghouas@gmail.com>
2018-10-26 12:57:12 -0700

    Add networkx dependency

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,4 +1,5 @@
-# Shell script to update/create the nomad conda environment. 
+# Shell script to update/create the nomad conda environment.
+# $ source setup/setup.sh 
 # Run this scrip every time you pull from github as dependancies may change.
 conda env update --name nomad --file setup/environment.yml
 source activate nomad 
