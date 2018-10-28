e2511e6eb4a9294abaf2426ba75cda666ff9974e
Alvin <alvin.ghouas@gmail.com>
2018-10-27 21:33:49 -0700

    Add shortes path solver

---
+++ core/placement/spsolver.py
@@ -0,0 +1,66 @@
+from abc import ABC, abstractmethod
+import networkx as nx
+from sys import maxsize
+from pprint import pprint
+
+class ShortestPathSolver(ABC):
+    """
+    Finds the shortest path of length k from sorce u to destination v in a directed edge weighted graph.
+    """
+    def __init__(self):
+        pass 
+    
+    @classmethod
+    @abstractmethod
+    def shortest_path(cls, graph, source, dest, k):
+        pass
+
+
+class DPShortesPathSolver(ShortestPathSolver):
+    @classmethod
+    def shortest_path(cls, graph, source, dest, k):
+        """
+         Returns shortest path for length from source to dest of length k. If no such path exists,
+         the function returns sys.maxsize, [].
+         Based on this implementation: https://www.geeksforgeeks.org/shortest-path-exactly-k-edges-directed-weighted-graph/ 
+        """
+        V = graph.number_of_nodes()
+        #Table to memoize results from DP. memo_table[i][j][e] corresponds to the shortest path 
+        # between node i and node j of length e
+        memo_table = [[[maxsize for k in range(k+1)] for j in range(V)] for i in range(V)]
+
+        #Tabled used to reconstruct the path 
+        path = [[[[] for k in range(k+1)] for j in range(V)] for i in range(V)] 
+        #Loop for num edges from 0 to k 
+        for e in range(k+1):
+            #for source
+            for s in range(V):
+                # for destination
+                for d in range(V):
+                    if(e == 0 and s == d):
+                        memo_table[s][d][e] = 0
+                        path[s][d][e].append(s)
+
+                    if(e == 1 and graph.has_edge(s,d)):
+                        memo_table[s][d][e] = graph[s][d][0]['weight']
+                        path[s][d][e] = [s,d] 
+
+                    if e > 1:
+                        #Loop over each neighbors of s
+                        for nbr in graph.neighbors(s):
+                            if nbr != d and memo_table[nbr][d][e-1] != maxsize:
+                                
+                                if memo_table[s][d][e] > graph[s][nbr][0]['weight'] + memo_table[nbr][d][e-1]:
+                                    memo_table[s][d][e] = graph[s][nbr][0]['weight'] + memo_table[nbr][d][e-1] 
+                                    path[s][d][e] = [s] + path[nbr][d][e-1]
+        
+        return memo_table[source][dest][k], path[source][dest][k]
+
+
+
+
+    
+  
+        
+
+    
