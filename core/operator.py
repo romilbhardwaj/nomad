ce9e3c359e512146dea78a079436a691ac2de798
Alvin <alvin.ghouas@gmail.com>
2018-10-28 18:11:56 -0700

    Update operator class. Add function msg_size_bits.

--- core/operator.py
+++ core/operator.py
@@ -2,9 +2,12 @@
 class Operator:
     def __init__(self, l, t, s):
         self._label = l #Name of operator
-        self._cloud_execution_time = t #Time to execute the operator in a cloud environment on a typical workload. Measured in seconds
+        self._cloud_execution_time = t #Time to execute the operator in a cloud environment on a typical workload. Measured in milliseconds
         self._output_msg_size = s # Size of operator output in Kilobytes assuming a typical input.
         self._next = None #Next operator in pipeline.
     
     def cloud_execution_time(self):
         return self._cloud_execution_time
+    
+    def msg_size_bits(self):
+        return self._output_msg_size * pow(10, 3) * 8
