137ec3f7dd360da0bf8ec1b826d061f724addc77
Alvin <alvin.ghouas@gmail.com>
2018-10-28 18:06:20 -0700

    Update transfer_time function in Link class. return value is now in milliseconds instead of seconds

--- core/graph/link.py
+++ core/graph/link.py
@@ -1,11 +1,10 @@
 class Link:
     def __init__(self, b, l):
         self._bandwidth = b #Mega bits/second(Mb/s)
-        self._latency = l   #milli seconds(ms)
+        self._latency = l   #milliseconds(ms)
     
     def transfer_time(self, bits):
-        """
-        Calculates the transfer time in seconds for a given number of bits
-        """
-        return bits / (self._bandwidth * pow(10,6)) + self._latency * pow(10, -3) 
+        """Calculates the transfer time in milliseconds for a given number of bits (ms)"""
+    
+        return bits / (self._bandwidth * pow(10,3)) + self._latency  
     
