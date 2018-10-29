fa43a6a87fa047b086ae71c53080e155a707d93b
Alvin <alvin.ghouas@gmail.com>
2018-10-28 18:08:12 -0700

    Update Link class. Added function add_operator and update_C_scaled. The processing time function returns the time it would take to execute a given operator on the current node.

--- core/graph/node.py
+++ core/graph/node.py
@@ -3,7 +3,16 @@ class Node:
     def __init__(self, label, C):
         self._label = label
         self._C = C
+        self._C_scaled = C
         self._num_operators = 0
     
     def processing_time(self, operator):
-        return operator.cloud_execution_time() / self._C
+        return operator.cloud_execution_time() / (self._C / (self._num_operators + 1))
+
+    def add_operator(self):
+        self._num_operators += 1
+        self._update_C_scaled()
+    
+    def _update_C_scaled(self):
+        self._C_scaled  = self._C / self._num_operators
+
