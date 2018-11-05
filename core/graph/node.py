95a9e6f53ddf71839229a440086f3b67080aeb59
Alvin <alvin.ghouas@gmail.com>
2018-11-05 13:47:31 -0800

    Update Processing_time function

--- core/graph/node.py
+++ core/graph/node.py
@@ -6,10 +6,11 @@ class Node:
         self._C_scaled = C
         self._num_operators = 0
     
-    def processing_time(self, operator):
-        return operator.cloud_execution_time() / (self._C / (self._num_operators + 1))
+    def processing_time(self, operator, ops_count):
+        return (operator.cloud_execution_time() * ops_count) / self._C
 
     def add_operator(self):
+        #TODO remove this function
         self._num_operators += 1
         self._update_C_scaled()
     
