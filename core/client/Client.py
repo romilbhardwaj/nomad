bf169c22bc4387d1bce5336ee1d1d75ca760bbd1
Romil <romil.bhardwaj@gmail.com>
2018-11-22 14:19:47 -0800

    Updates to client to use dill for method serialization and queue.Queues for message queues.

--- core/client/Client.py
+++ core/client/Client.py
@@ -4,9 +4,13 @@ import threading
 import xmlrpc
 import time
 import sys
+from queue import Queue
+
+import dill
 
 from nomad.core.utils.LoggerWriter import LoggerWriter
 from nomad.utils.RPCServerThreads import RPCServerThread
+import nomad.core.config.ClientConfig as ClientConfig
 
 # ====== BEGIN SETUP LOGGING =========
 
@@ -16,8 +20,8 @@ logger = logging.getLogger()
 logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s] [%(name)-4s] %(message)s")
 
 # Setup logging to file
-os.makedirs(CONFIG.DEFAULT_LOG_DIR, exist_ok=True)
-fileHandler = logging.FileHandler("{0}/{1}".format(CONFIG.DEFAULT_LOG_DIR, CONFIG.CLIENT_LOG_FILE_NAME))
+os.makedirs(ClientConfig.DEFAULT_LOG_DIR, exist_ok=True)
+fileHandler = logging.FileHandler("{0}/{1}".format(ClientConfig.DEFAULT_LOG_DIR, ClientConfig.CLIENT_LOG_FILE_NAME))
 fileHandler.setFormatter(logFormatter)
 logger.addHandler(fileHandler)
 
@@ -42,25 +46,52 @@ class NomadClient(object):
 
         def work(self):
             while (True):
-                for message in self.incoming_queue:
-                    kwargs = message.get_args()
-                    output = self.operator_func(kwargs)
-                    self.outgoing_queue.append(output)
+                message = self.incoming_queue.get()
+                kwargs = message.get_args()
+                output = self.operator_func(kwargs)
+                self.outgoing_queue.put(output)
 
         def run(self):
             self.work()
 
     class ForwardingThread(threading.Thread):
-        def __init__(self, outgoing_queue):
-            raise NotImplementedError
+        def __init__(self, outgoing_queue, next_operator_rpc_uri):
+            threading.Thread.__init__(self)
+            self.daemon = False
 
-    def __init__(self, guid, manager_rpc_uri, operator_func):
+            self.outgoing_queue = outgoing_queue
+            self.next_operator_rpc_uri = next_operator_rpc_uri
+            self.next_operator_rpc_proxy = xmlrpc.client.ServerProxy(self.next_operator_rpc_uri, allow_none=True)
+
+
+        def work(self):
+            while(1):
+                out_message = self.outgoing_queue.get() # This is a blocking call
+                self.next_operator_rpc_proxy.submit_message(out_message)
+
+        def run(self):
+            self.work()
+
+    def __init__(self, guid, manager_rpc_uri, operator_path=None):
+        '''
+        :param guid: GUID for this client
+        :type guid: str
+        :param manager_rpc_uri: RPC URI for the manager
+        :type manager_rpc_uri: str
+        :param operator_path: path to the dill file of the operator
+        :type operator_path: str
+        '''
         self.guid = guid
         self.manager_rpc_uri = manager_rpc_uri
-        self.operator_func = operator_func
+        if self.operator_path is None:
+            self.operator_path = ClientConfig.DEAFULT_OPERATOR_DILL_PATH
+        else:
+            self.operator_path = operator_path
 
-        self.incoming_queue = []
-        self.outgoing_queue = []
+        self.operator = self._load_operator(self.operator_path)
+
+        self.incoming_queue = Queue(maxsize=ClientConfig.MAX_INCOMING_QUEUE_SIZE)
+        self.outgoing_queue = Queue(maxsize=ClientConfig.MAX_OUTGOING_QUEUE_SIZE)
 
         # Setup manager RPCProxy
         self.standalone_mode = False    # When no server uri specified.
@@ -82,8 +113,8 @@ class NomadClient(object):
         while not is_rpc_ready:
             is_rpc_ready = self.test_self_rpcserver()
             if not is_rpc_ready:
-                logger.info("RPC Server is not ready. Sleeping for %d" % CONFIG.RPC_INIT_WAIT_INTERVAL)
-                time.sleep(CONFIG.RPC_INIT_WAIT_INTERVAL)
+                logger.info("RPC Server is not ready. Sleeping for %d" % ClientConfig.RPC_INIT_WAIT_INTERVAL)
+                time.sleep(ClientConfig.RPC_INIT_WAIT_INTERVAL)
             else:
                 logger.info("RPC is ready. Proceed to notifying nomad master.")
 
@@ -111,3 +142,7 @@ class NomadClient(object):
 
     def submit_message(self, message):
         self.incoming_queue.append(message)
+
+    @staticmethod
+    def _load_operator(operator_path):
+        return dill.load(operator_path)
