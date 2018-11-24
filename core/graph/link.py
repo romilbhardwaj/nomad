bd2310a0839510447a62ce067c0fff8d96041743
Alvin <alvin.ghouas@gmail.com>
2018-11-23 16:57:38 -0800

    Add from, to paramas to Link class

--- core/graph/link.py
+++ core/graph/link.py
@@ -1,7 +1,9 @@
 class Link:
-    def __init__(self, b, l):
+    def __init__(self, f, t, b, l):
         self._bandwidth = b # bytes/second(Mb/s)
         self._latency = l   # seconds(ms)
+        self._from = f
+        self._to = t
     
     def transfer_time(self, bytes):
         """Calculates the transfer time in milliseconds for a given number of bytes (ms)"""
---
+++ tests/core/types/test_cluster.py
@@ -0,0 +1,22 @@
+import unittest
+from core.universe.cluster import Cluster
+class TestClusterMethods(unittest.TestCase):
+    def setUp(self):
+        self.node_info = open("tests/types/nodes.csv")
+        self.link_info = open('tests/types/links.csv')
+        self.pipeline_info = open('tests/types/pipeline.csv')
+        self.graph_topo = open('tests/types/graph_topology.txt')
+
+    def tearDown(self):
+        self.node_info.close()
+        self.link_info.close()
+        self.pipeline_info.close()
+        self.graph_topo.close()
+
+    def test_create_universe(self):
+        universe = Universe.create_universe(self.node_info, self.link_info,  self.graph_topo)
+        self.assertTrue(universe.cluster != None)
+
+
+if __name__ == '__main__':
+    unittest.main()
