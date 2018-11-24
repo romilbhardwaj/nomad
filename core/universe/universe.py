568c40fa021d0cf7c09b7fff7fee2b2aea5e8f0c
Alvin <alvin.ghouas@gmail.com>
2018-11-23 17:00:40 -0800

    Add create_universe method

--- core/universe/universe.py
+++ core/universe/universe.py
@@ -1,3 +1,5 @@
+from core.types.cluster import Cluster
+
 class Universe(object):
     def __init__(self, cluster):
         """
@@ -16,4 +18,32 @@ class Universe(object):
                     next_operator_instance = pipeline.operator_instance[pipeline.operators.index(operator_to_find) + 1]  # Assuming ordering on operator_instance is the same as operators
                     return next_operator_instance
         raise Exception("No next operator.")
+
+    @classmethod
+    def create_universe(cls, node_file, links_file, graph_topo_file):
+        """
+
+        :param node_file: JSON file containing node info
+        :param links_file: JSON file containing link info
+        :param graph_topo_file: File containing adjacency list. One node pair per line.
+        :return:
+        """
+        cluster = Cluster(node_file, links_file, graph_topo_file)
+        return  cls(cluster)
+
+    def add_pipeline(self):
+        pass
+
+    def save_scheduling_decision(self):
+
+        pass
+
+    def update_network_profiling(self, links):
+        #update the cluster info
+        pass
+
+    def update_pipeline_profiling(self, pipeline):
+        #update piepline profile.
+
+        pass
 
