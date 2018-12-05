e4d9f26fe0e589b9e3f34f74810e787d2120b07e
Alvin <alvin.ghouas@gmail.com>
2018-12-04 18:42:27 -0800

    Uncoment

--- nomad/tests/core/master/test_master.py
+++ nomad/tests/core/master/test_master.py
@@ -2,11 +2,9 @@ import unittest
 import xmlrpc
 import subprocess
 
-
 class TestMaster(unittest.TestCase):
     def setUp(self):
         self.master_process = subprocess.Popen(["python", "../../../core/master/master_launcher.py", "--rpc_port=20000"])
-
 
     def test_get_next_op(self):
         manager_rpc = xmlrpc.client.ServerProxy("http://127.0.0.1:20000", allow_none=True)
