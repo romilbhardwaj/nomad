a207b8b3cef2e31c082ca92a4e362ff6769217be
romilbhardwaj <romil.bhardwaj@gmail.com>
2018-12-09 19:45:18 -0800

    Update to make images work offline, if possible.

--- nomad/core/master/kubernetes_api.py
+++ nomad/core/master/kubernetes_api.py
@@ -154,7 +154,7 @@ class KubernetesAPI(object):
         container = client.V1Container(name=name, image=image, tty=True,
                                        env=container_envs, ports=container_ports,
                                        volume_mounts=volume_mounts, security_context=security_context,
-                                       image_pull_policy="Always")
+                                       image_pull_policy="Never")
         return container
 
     def _create_kube_service(self, k8s_id, ports, namespace=KubernetesConfig.K8S_NAMESPACE):
--- nomad/rpi_demo.py
+++ nomad/rpi_demo.py
@@ -3,7 +3,7 @@ import xmlrpc.client
 rpc = xmlrpc.client.ServerProxy("http://127.0.0.1:30000", allow_none=True)
 print(str(rpc.system.listMethods()))
 
-fns = ["read_picamera", "downsample_image", "get_face_boundingbox", "get_face_emotion"]
+fns = ["read_picamera_downsampled", "downsample_image", "get_face_boundingbox", "get_face_emotion"]
 end = "romilbx1yoga"
 start = "raspberrypi"
 rpc.submit_pipeline(fns, start, end, 'demo')
