83daa24f2842ce3db393f2249310bf7bae514242
Alvin <alvin.ghouas@gmail.com>
2019-02-08 11:23:38 -0800

    Change Operator init signature to take list of images instead of files

--- nomad/core/types/operator.py
+++ nomad/core/types/operator.py
@@ -2,10 +2,10 @@ from nomad.core.config import ClientConfig
 
 
 class Operator:
-    def __init__(self, guid, l='', fn_file='', t=0, s=0, is_first=False, is_final=False):
+    def __init__(self, guid, l='', fn_image='', t=0, s=0, is_first=False, is_final=False):
         self.guid = guid
         self._label = l #Name of operator
-        self._fn_file = fn_file
+        self._fn_image = fn_image
         self._cloud_execution_time = t #Time to execute the operator in a cloud environment on a typical workload. Measured in seconds
         self._output_msg_size = s # Size of operator output in bytes assuming a typical input.
         self._next = None # GUID of next op in pipeline
