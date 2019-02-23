d4969378ab4bbd1e79d2a8820a9418cc0f516486
Romil <romil.bhardwaj@gmail.com>
2019-02-22 19:06:53 -0800

    remove junk from addition.py

--- nomad/tests/e2e/addition.py
+++ nomad/tests/e2e/addition.py
@@ -17,24 +17,3 @@ fns = [source, square]
 end = "kube-master"
 start = "kube-node-1"
 nomad.submit_pipeline(fns, start, end, 'demo', connection_str)
-
-import docker
-import os
-client = docker.from_env()
-i=1
-base_dir = '/tmp/op_%d/' % i
-file_name = base_dir + 'operator.pickle'
-#TODO: the docker repo should be loaded from a config file
-tag = "lab11nomad/operators:%s_op_%d_img" % ("demo", i)
-
-#build image using client base image
-build_src_path = '/tmp'
-pickle_rel_path = os.path.relpath(file_name, build_src_path)
-
-
-docker_image, build_log = client.images.build(tag=tag, path='/tmp', buildargs={'PYTHON_PICKLE_PATH': pickle_rel_path}, rm=True)
-print("Build result: \n%s" % str(docker_image))
-for line in build_log:
-    print(line)
-
-#push_result = client.images.push(repository=tag, auth_config={'username': DockerConfig.USERNAME, 'password': DockerConfig.PASSWORD})
