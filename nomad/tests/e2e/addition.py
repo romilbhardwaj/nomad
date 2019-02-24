45ed26420d4ab74eff660c8f5fd176168ad0be73
Romil <romil.bhardwaj@gmail.com>
2019-02-23 22:21:16 -0800

    Update addition

--- nomad/tests/e2e/addition.py
+++ nomad/tests/e2e/addition.py
@@ -6,6 +6,10 @@ connection_str = "http://127.0.0.1:30000"
 rpc = xmlrpc.client.ServerProxy(connection_str, allow_none=True)
 print(str(rpc.system.listMethods()))
 
+def write_to_file(x):
+    with open("/tmp/output.txt", 'a') as f:
+        f.write(str(x) + "\n")
+
 def square(x):
     return x*x
 
@@ -13,7 +17,7 @@ def source():
     import random
     return random.randint(0,10)
 
-fns = [source, square]
+fns = [source, square, write_to_file]
 end = "kube-master"
 start = "kube-node-1"
 nomad.submit_pipeline(fns, start, end, 'demo', connection_str)
