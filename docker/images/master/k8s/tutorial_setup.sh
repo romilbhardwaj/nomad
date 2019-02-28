c32643adcc4d1d99ec72b1cea532d688b4c55185
Alvin <alvin.ghouas@gmail.com>
2019-02-28 12:25:16 -0800

    Add setup script for steps 1-3 of tutorial

---
+++ docker/images/master/k8s/tutorial_setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Run the script
+chmod +x dind-cluster-v1.12.sh
+./dind-cluster-v1.12.sh up
+# Add kubectl binaries to PATH
+export PATH="$HOME/.kubeadm-dind-cluster:$PATH"
+echo "export PATH=\"$HOME/.kubeadm-dind-cluster:$PATH\"" >> ~/.bashrc
+
+chmod +x init.sh startup.sh
+# init.sh initializes the nomad master namespace in Kubernetes. This needs to be run everytime a new kuberenetes cluster is used.
+./init.sh
+
+# startup.sh launches the nomad master deployment on the cluster
+./startup.sh
+
+#Wait for master container to start running.
+while !(kubectl get pod -l "app=nomadmaster" | grep "Running");
+do
+    echo "Waiting for master to start running..."
+    sleep 2
+done
+
+#Get shell at running container and launch python.
+kubectl exec -it $(kubectl get pod -l "app=nomadmaster" -o jsonpath='{.items[0].metadata.name}') -- bash -c "python"
