f0419b1d64c976b364d7eb098756af398aed4d61
Romil <romil.bhardwaj@gmail.com>
2019-03-07 11:44:07 -0800

    Move tutorial scripts

--- tutorial/tutorial_setup.sh
+++ tutorial/tutorial_setup.sh
@@ -5,19 +5,18 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     brew install md5sha1sum
 fi
 
-# Run the script
-# chmod +x dind-cluster-v1.12.sh
-./dind-cluster-v1.12.sh up
+# Run the DIND script
+../docker/images/master/k8s/dind-cluster-v1.12.sh up
+
 # Add kubectl binaries to PATH
 export PATH="$HOME/.kubeadm-dind-cluster:$PATH"
 # echo "export PATH=\"$HOME/.kubeadm-dind-cluster:$PATH\"" >> ~/.bashrc
 
-# chmod +x init.sh startup.sh
 # init.sh initializes the nomad master namespace in Kubernetes. This needs to be run everytime a new kuberenetes cluster is used.
-./init.sh
+../docker/images/master/k8s/init.sh
 
 # startup.sh launches the nomad master deployment on the cluster
-./startup.sh
+../docker/images/master/k8s/startup.sh
 
 #Wait for master container to start running.
 while !(kubectl get pod -l "app=nomadmaster" | grep "Running");
--- tutorial/tutorial_shutdown.sh
+++ tutorial/tutorial_shutdown.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-./dind-cluster-v1.12.sh clean
+../docker/images/master/k8s/dind-cluster-v1.12.sh clean
 docker rm --force $(docker ps -aq)
 docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'lab11*')
