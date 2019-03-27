01ae29d2ea7285b7cdc397593ce56f548bf22a18
Alvin <alvin.ghouas@gmail.com>
2019-03-27 15:37:12 -0700

    Update master launcher with qemu-user-static emulator

--- nomad/core/master/master_launcher.py
+++ nomad/core/master/master_launcher.py
@@ -1,5 +1,5 @@
 import argparse
-
+import docker
 from nomad.core.config import ClientConfig
 from nomad.core.master.master import Master
 from nomad.core.universe.universe import Universe
@@ -13,6 +13,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Launches the nomad client.')
     parser.add_argument('--rpc_port', type=int, help='RPC port for the master.', default=ClientConfig.RPC_DEFAULT_PORT)
     args = parser.parse_args()
+    client = docker.from_env()
+    client.containers.run(image='mulitarch/qemu-user-static:register', remove=True, privileged=True)
     print("Starting nomad master")
     master_args = []
     master_kwargs = {'master_rpc_port': int(args.rpc_port)}#,
