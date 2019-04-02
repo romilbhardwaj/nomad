80fd4ea4789bfdca66664cbf87bb55c82ef99b6d
Alvin <alvin.ghouas@gmail.com>
2019-04-02 16:57:23 -0700

    Fix bug in master launcher

--- nomad/core/master/master_launcher.py
+++ nomad/core/master/master_launcher.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     parser.add_argument('--rpc_port', type=int, help='RPC port for the master.', default=ClientConfig.RPC_DEFAULT_PORT)
     args = parser.parse_args()
     client = docker.from_env()
-    client.containers.run(image='mulitarch/qemu-user-static:register', remove=True, privileged=True)
+    client.containers.run(image='multiarch/qemu-user-static:register', remove=True, privileged=True)
     print("Starting nomad master")
     master_args = []
     master_kwargs = {'master_rpc_port': int(args.rpc_port)}#,
