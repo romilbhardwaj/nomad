749a2367a85f6f7585cb135e357ee4690ee81e2f
Alvin <alvin.ghouas@gmail.com>
2019-04-05 15:53:22 -0700

    Add fn_images attr to Operator class. This attr is a dict with the following structure: { arch: img_tag }

--- nomad/core/types/operator.py
+++ nomad/core/types/operator.py
@@ -5,10 +5,10 @@ from nomad.core.utils.helpers import construct_xmlrpc_addr
 
 
 class Operator:
-    def __init__(self, guid, l='', fn_image='', t=0, s=0, is_first=False, is_final=False):
+    def __init__(self, guid, l='', fn_images=None, t=0, s=0, is_first=False, is_final=False):
         self.guid = guid
         self._label = l #Name of operator
-        self._fn_image = fn_image
+        self._fn_images = fn_images # dictionary mapping arch -> image string
         self._cloud_execution_time = t #Time to execute the operator in a cloud environment on a typical workload. Measured in seconds
         self._output_msg_size = s # Size of operator output in bytes assuming a typical input.
         self._next = None # GUID of next op in pipeline
