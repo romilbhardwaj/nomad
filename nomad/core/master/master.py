684e0fb53e07a45bd7f601ac46ac4ca9a3e4d12f
Alvin <alvin.ghouas@gmail.com>
2019-04-12 13:06:24 -0700

    Move image slection logic to Master.instantiate_pipeline

--- nomad/core/master/master.py
+++ nomad/core/master/master.py
@@ -287,8 +287,10 @@ class Master(object):
         for operator_instance in operator_instances:
             node = self.universe.get_node(operator_instance.node_id)
             #TODO: select image based on node arch
-            images = self.universe.get_operator(operator_instance.operator_guid)._fn_images
-            k8s_service, k8s_job, image = self.KubernetesAPI.create_kube_service_and_job(operator_instance, images=images, architecture=node._architecture)
+            architecture = node._architecture
+            #Select the correct image
+            image = self.universe.get_operator(operator_instance.operator_guid).get_image(architecture)
+            k8s_service, k8s_job, image = self.KubernetesAPI.create_kube_service_and_job(operator_instance, image=image)
             operator_instance.update_ip(k8s_service.spec.cluster_ip)    # update the ip from kubernetes
             operator_instance.update_image(image)
             operator_instance.update_state('running')
